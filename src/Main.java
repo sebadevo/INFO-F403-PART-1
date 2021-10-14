4be8955989a005678b37ff161fca4f7062143ba4
Tarik <Tarik.Kalai@ulb.be>
2021-10-14 14:53:24 +0200

    set up Java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-Public Class Main {
-    private ArrayList<int> number_of_variable;
+public class Main {
     
-
+    //private ArrayList<int> number_of_variable = new ArrayList<int>;
+    
     public static void main(String[] args) {
-        launch (args);
+        //launch(args);
+        System.out.println("hello world");
     }
 }
